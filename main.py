968108c4fc46e7552279c7d4eda6d52658c541fe
Iago Breno <ibacaraujo@gmail.com>
2019-12-11 16:34:04 -0300

    Configure arguments

---
+++ main.py
@@ -0,0 +1,64 @@
+# Deep Convolutional Generative Adversarial Networks
+
+## Notation
+# x is the data representing an image
+# D(x) is the discriminator network which outputs probability that x came from training data
+# z is a latent space vector sampled from a normal distribution
+# G(z) is the generator network which maps the latent vector z to data-space
+# D(G(z)) is the probability that the output of the generator G is a real image
+
+# D tries to maximize the probability it correctly classifies reals and fakes (log(D(x)))
+# G tries to minimize the probability that D will predict its outputs are fake (log(1 - D(G(x))))
+
+from __future__ import print_function
+import argparse
+import os
+import random
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.utils.data
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', required=True, help='cifar10 | lsun | mnist | imagenet | folder | lwf | fake')
+parser.add_argument('--dataroot', required=True, help='path to dataset')
+parser.add_argument('--workers', type=int, help='number of data loading workers', default=2)
+parser.add_argument('batchSize', type=int, default=64, help='input batch size')
+parser.add_argument('--imageSize', type=int, default=64, help='the height / width of the input image to network')
+parser.add_argument('--nz', type=int, default=100, help='size of the latent vector z')
+parser.add_argument('--ngf', type=int, default=64)
+parser.add_argument('--ndf', type=int, default=64)
+parser.add_argument('--niter', type=int, default=25, help='number of epochs to train for')
+parser.add_argument('--lr', type=float, default=0.0002, help='learning rate, default=0.0002')
+parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
+parser.add_argument('--cuda', action='store_true', help='enables_cuda')
+parser.add_argument('--ngpu', type=int, default=1, help='number of GPUs to use')
+parser.add_argument('--netG', default='', help='path to netG (to continue training)')
+parser.add_argument('--netD', default='', help='path to netD (to continue training)')
+parser.add_argument('--outf', default='.', help='folder to output images and model checkpoints')
+parser.add_argument('--manualSeed', type=int, help='manual seed')
+parser.add_argument('--classes', default='bedroom', help='comma separated list of classes for the lsun data set')
+
+opt = parser.parse_args()
+print(opt)
+
+try:
+    os.makedirs(opt.outf)
+except OSError:
+    pass
+
+if opt.manualSeed is None:
+    opt.manualSeed = random.randint(1, 10000)
+print('Random Seed: ', opt.manualSeed)
+random.seed(opt.manualSeed)
+torch.manual_seed(opt.manualSeed)
+
+cudnn.benchmark = True
+
+if torch.cuda.is_available() and not opt.cuda:
+    print('WARNING: You have a CUDA device, so you should probably run with --cuda')
