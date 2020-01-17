c429d93bb06bcb029d50b46bfe32fa96e6145ed4
Iago Breno <ibacaraujo@gmail.com>
2020-01-16 21:06:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -89,10 +89,10 @@ ndf = int(opt.ndf)
 def weights_init(m):
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
-        m.weight.data.normal_(0.0, 0.02)
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
     elif classname.find('BatchNorm') != -1:
-        m.weight.data.normal_(1.0, 0.02)
-        m.bias.data.fill_(0)
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
 
 # Generator
 # Converting z to data-space which means creating RGB images with the same size as the training
